a6f9e2ef5a817c28512059853ad522f74e845c44
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2019-01-24 10:24:08 +0100

    Première version du module

--- hadoop-map-reduce/hadoop-agent/src/main/c/hadooptrace.c
+++ hadoop-map-reduce/hadoop-agent/src/main/c/hadooptrace.c
@@ -6,17 +6,21 @@
 #include "ezTrace_HadoopTrace.h"
 
 JNIEXPORT void JNICALL Java_ezTrace_HadoopTrace_mapProlog(JNIEnv* env, jobject obj) {
-    printf("map from thread %d ...\n", pthread_self());
+    printf("map from thread %lu ...\n", pthread_self());
+    EZTRACE_EVENT1(EZTRACE_HADOOP_MAP_PROLOG, pthread_self());
 }
 
 JNIEXPORT void JNICALL Java_ezTrace_HadoopTrace_mapEpilog(JNIEnv* env, jobject obj) {
     printf("map ok\n");
+    EZTRACE_EVENT1(EZTRACE_HADOOP_MAP_EPILOG, pthread_self());
 }
 
 JNIEXPORT void JNICALL Java_ezTrace_HadoopTrace_reduceProlog(JNIEnv* env, jobject obj) {
-    printf("reduce from thread %d ...\n", pthread_self());
+    printf("reduce from thread %lu ...\n", pthread_self());
+    EZTRACE_EVENT1(EZTRACE_HADOOP_REDUCE_PROLOG, pthread_self());
 }
 
 JNIEXPORT void JNICALL Java_ezTrace_HadoopTrace_reduceEpilog(JNIEnv* env, jobject obj) {
     printf("reduce ok\n");
+    EZTRACE_EVENT1(EZTRACE_HADOOP_REDUCE_EPILOG, pthread_self());
 }
