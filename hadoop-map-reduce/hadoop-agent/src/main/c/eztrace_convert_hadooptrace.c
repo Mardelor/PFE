4e4fe19f3e7253a775d903bc0b64752cb999a529
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2019-01-25 17:15:16 +0100

    Implémentation du eztrace_convert & ajout dans le makefile

--- hadoop-map-reduce/hadoop-agent/src/main/c/eztrace_convert_hadooptrace.c
+++ hadoop-map-reduce/hadoop-agent/src/main/c/eztrace_convert_hadooptrace.c
@@ -84,7 +84,7 @@ static float last_start_time_reduce = 0;
  * 0 otherwise.
  */
 int handle_hadoop_stats(eztrace_event_t *ev) {
-    /* depending on the event code, let's count the number of calls to function1 and function2
+    /* depending on the event code, let's count the number of calls to map and reduce
      * for both function, compute the average duration of the function call
      */
     switch (LITL_READ_GET_CODE(ev)) {
@@ -93,7 +93,7 @@ int handle_hadoop_stats(eztrace_event_t *ev) {
             last_start_time_map = CURRENT;
             break;
         case EZTRACE_HADOOP_MAP_EPILOG:
-            /* WARNING: this does not support support reentrant calls to function1.
+            /* WARNING: this does not support support reentrant calls to map.
              */
             total_time_in_map += CURRENT - last_start_time_map;
             break;
@@ -103,7 +103,7 @@ int handle_hadoop_stats(eztrace_event_t *ev) {
             last_start_time_reduce = CURRENT;
             break;
         case EZTRACE_HADOOP_REDUCE_EPILOG:
-            /* WARNING: this does not support support reentrant calls to function2.
+            /* WARNING: this does not support support reentrant calls to reduce.
              */
             total_time_in_reduce += CURRENT - last_start_time_reduce;
             break;
@@ -117,21 +117,111 @@ int handle_hadoop_stats(eztrace_event_t *ev) {
 
 /* Print the results of statistics.
  */
-void print_example_stats() {
+void print_hadoop_stats() {
     printf("\nLibHadoop:\n");
     printf("-----------\n");
 
     if (nb_map_calls) {
-        /* Only print this when calls to function1 were detected */
+        /* Only print this when calls to map were detected */
         printf("%d calls to Map\n", nb_map_calls);
         printf("\taverage duration: %f us\n",
                total_time_in_map / nb_map_calls);
     }
 
     if (nb_reduce_calls) {
-        /* Only print this when calls to function2 were detected */
+        /* Only print this when calls to reduce were detected */
         printf("%d calls to Reduce\n", nb_reduce_calls);
         printf("\taverage duration: %f us\n",
                total_time_in_reduce / nb_reduce_calls);
     }
+}
+
+/*
+ * This function is called by eztrace_convert for each event to handle.
+ * It shall return one if the event was handled successfully or zero otherwise.
+ */
+int handle_hadoop_events(eztrace_event_t *ev) {
+    switch (LITL_READ_GET_CODE(ev)) {
+        case EZTRACE_HADOOP_MAP_PROLOG:
+            handle_hadoop_map_entry();
+            break;
+        case EZTRACE_HADOOP_MAP_EPILOG:
+            handle_hadoop_map_exit();
+            break;
+
+        case EZTRACE_HADOOP_REDUCE_PROLOG:
+            handle_hadoop_reduce_entry();
+            break;
+        case EZTRACE_HADOOP_REDUCE_EPILOG:
+            handle_hadoop_reduce_exit();
+            break;
+
+        default:
+            /* The event was not handled */
+            return 0;
+    }
+    return 1;
+}
+
+/* This function is called by handle_example_events when reaching
+ * the EZTRACE_HADOOP_MAP_PROLOG event.
+ */
+void handle_hadoop_map_entry() {
+    /* FUNC_NAME is used for debugging. If -v is passed to eztrace_convert,
+     * FUNC_NAME prints the name of the function
+     */
+    FUNC_NAME;
+
+    /* Initialize a char* thread_id and generate a string that
+     * identifies the thread that issued the current event
+     */
+//  DECLARE_THREAD_ID_STR(thread_id, CUR_INDEX, CUR_THREAD_ID;
+    DECLARE_THREAD_ID_STR(thread_id, CUR_INDEX, CUR_THREAD_ID);
+    /* Update the state of the thread in the output trace. */CHANGE()
+        pushState(CURRENT, "ST_Thread", thread_id, "map_alias");
+
+    free(thread_id);
+}
+
+/* This function is called by handle_example_events when reaching
+ * the EZTRACE_HADOOP_MAP_EPILOG event.
+ */
+void handle_hadoop_map_exit() {
+    FUNC_NAME;
+    DECLARE_THREAD_ID_STR(thread_id, CUR_INDEX, CUR_THREAD_ID);
+
+    /* Restore the state of the thread to its previous state in the
+     * output trace.
+     */CHANGE()
+        popState(CURRENT, "ST_Thread", thread_id);
+
+    free(thread_id);
+}
+
+/* This function is called by handle_example_events when reaching
+ * the EZTRACE_HADOOP_REDUCE_PROLOG event.
+ */
+void handle_hadoop_reduce_entry() {
+    FUNC_NAME;
+    DECLARE_THREAD_ID_STR(thread_id, CUR_INDEX, CUR_THREAD_ID);
+
+    /* Update the state of the thread in the output trace. */CHANGE()
+        pushState(CURRENT, "ST_Thread", thread_id, "function2_alias");
+
+    free(thread_id);
+}
+
+/* This function is called by handle_example_events when reaching
+ * the EZTRACE_HADOOP_REDUCE_EPILOG event.
+ */
+void handle_hadoop_reduce_exit() {
+    FUNC_NAME;
+    DECLARE_THREAD_ID_STR(thread_id, CUR_INDEX, CUR_THREAD_ID);
+
+    /* Restore the state of the thread to its previous state in the
+     * output trace.
+     */CHANGE()
+        popState(CURRENT, "ST_Thread", thread_id);
+
+    free(thread_id);
 }
