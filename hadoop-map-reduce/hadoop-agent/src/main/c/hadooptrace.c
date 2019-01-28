5e09db961a640883fd88f44c52f2269ad96b1bb9
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2019-01-28 11:15:01 +0100

    Mise à jour gitignore & retrait des print pour hadoop trace

--- hadoop-map-reduce/hadoop-agent/src/main/c/hadooptrace.c
+++ hadoop-map-reduce/hadoop-agent/src/main/c/hadooptrace.c
@@ -6,22 +6,18 @@
 #include "ezTrace_HadoopTrace.h"
 
 JNIEXPORT void JNICALL Java_ezTrace_HadoopTrace_mapProlog(JNIEnv* env, jobject obj) {
-    printf("map from thread %lu ...\n", pthread_self());
     EZTRACE_EVENT0(EZTRACE_HADOOP_MAP_PROLOG);
 }
 
 JNIEXPORT void JNICALL Java_ezTrace_HadoopTrace_mapEpilog(JNIEnv* env, jobject obj) {
-    printf("map ok\n");
     EZTRACE_EVENT0(EZTRACE_HADOOP_MAP_EPILOG);
 }
 
 JNIEXPORT void JNICALL Java_ezTrace_HadoopTrace_reduceProlog(JNIEnv* env, jobject obj) {
-    printf("reduce from thread %lu ...\n", pthread_self());
     EZTRACE_EVENT0(EZTRACE_HADOOP_REDUCE_PROLOG);
 }
 
 JNIEXPORT void JNICALL Java_ezTrace_HadoopTrace_reduceEpilog(JNIEnv* env, jobject obj) {
-    printf("reduce ok\n");
     EZTRACE_EVENT0(EZTRACE_HADOOP_REDUCE_EPILOG);
 }
 
